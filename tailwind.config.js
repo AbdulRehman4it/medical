dde4038bf9717c3004157655c18e8628574e9401
Abdul Rehman <abdulrehman226721skp@gmail.com>
2025-02-25 12:42:23 +0500

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
